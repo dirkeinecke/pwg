26ca43f6d08a52f4cc231fd74e479ea6921d0d31
Dirk Einecke <dirk.einecke@gmail.com>
2019-04-06 18:52:02 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
 # pwg
+
+[![Build Status](https://travis-ci.org/dirkeinecke/pwg.svg?branch=master)](https://travis-ci.org/dirkeinecke/pwg)
+[![Crate](https://img.shields.io/crates/v/pwg.svg)](https://crates.io/crates/pwg)
+[![Crates.io (Downloads)](https://img.shields.io/crates/d/pwg.svg)](https://crates.io/crates/pwg)
+[![API](https://docs.rs/pwg/badge.svg)](https://docs.rs/pwg)
+![License](https://img.shields.io/crates/l/pwg.svg)
+[![Gitter](https://badges.gitter.im/pwg-rs/community.svg)](https://gitter.im/pwg-rs/community?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
+
+With `pwg` you can easily create random passwords. You can specify how long the password should be and whether it should also contain capital letters, numbers and special characters.
+
+## Usage
+
+Add this to your `Cargo.toml`:
+
+```toml
+[dependencies]
+pwg = "0.1"
+```
+
+Add this to your `*.rs` file:
+
+```rust
+extern crate pwg;
+```
+
+Now you can proceed as follows:
+
+### Example 1
+
+```rust
+/*
+  A password with 20 characters is generated.
+  It consists of lowercase letters (default),
+  uppercase letters, numbers and symbols.
+*/
+let password = pwg::new(20, &["uppercase", "numbers", "symbols"]);
+println!("{}", password);
+```
+
+### Example 2
+
+```rust
+/* A password with 10 characters (lower case letters) is generated. */
+let password = pwg::new(10, &[]);
+println!("{}", password);
+```
+
+### Example 3
+
+```rust
+/*
+  A password with 20 characters is generated.
+  It contains of lowercase letters (default) and uppercase letters.
+*/
+let password = pwg::new(20, &["uppercase"]);
+println!("{}", password);
+```
+
+### Example 4
+
+```rust
+/*
+  A password with 20 characters is generated.
+  It contains of lowercase letters (default) and numbers.
+*/
+let password = pwg::new(20, &["numbers"]);
+println!("{}", password);
+```
+
+### Example 5
+
+```rust
+/*
+  A password with 20 characters is generated.
+  It contains of lowercase letters (default) and symbols.
+*/
+let password = pwg::new(20, &["symbols"]);
+println!("{}", password);
+```
+
+## Getting help
+
+If you have questions or problems with `pwg`, then we are happy to respond to [GitHub issues](https://github.com/dirkeinecke/pwg/issues/new) or come chat with us on our [Gitter channel](https://gitter.im/pwg-rs/community) - if you have any questions about the project, or just want to say hi!
+
+## License
+
+`pwg` is distributed under the terms of the MIT license.
+
+See [LICENSE](LICENSE) for details.
---
+++ src/lib.rs
@@ -0,0 +1,127 @@
+//! # pwg
+//!
+//! With `pwg` you can easily create random passwords. You can specify how long the password should be and whether it should also contain capital letters, numbers and special characters.
+
+extern crate rand;
+
+use rand::prelude::*;
+
+/// The ingredients from which the password is generated are provided here.
+///
+/// - lowercase letters
+/// - upper-case letters
+/// - numbers
+/// - symbols (special characters)
+struct Pool<'a> {
+    lowercase_letters: [&'a str; 26],
+    uppercase_letters: [&'a str; 26],
+    numbers: [&'a str; 10],
+    symbols: [&'a str; 8],
+}
+
+/// Easily create random passwords. You can specify how long the password should be and whether it should also contain capital letters, numbers and special characters.
+///
+/// # Examples
+///
+/// ```rust
+/// /*
+///   A password with 20 characters is generated.
+///   It consists of lowercase letters (default),
+///   uppercase letters, numbers and symbols.
+/// */
+/// let password = pwg::new(20, &["uppercase", "numbers", "symbols"]);
+/// println!("{}", password);
+/// ```
+///
+/// ```rust
+/// /* A password with 10 characters (lower case letters) is generated. */
+/// let password = pwg::new(10, &[]);
+/// println!("{}", password);
+/// ```
+///
+/// ```rust
+/// /*
+///   A password with 20 characters is generated.
+///   It contains of lowercase letters (default) and uppercase letters.
+/// */
+/// let password = pwg::new(20, &["uppercase"]);
+/// println!("{}", password);
+/// ```
+///
+/// ```rust
+/// /*
+///   A password with 20 characters is generated.
+///   It contains of lowercase letters (default) and numbers.
+/// */
+/// let password = pwg::new(20, &["numbers"]);
+/// println!("{}", password);
+/// ```
+///
+/// ```rust
+/// /*
+///   A password with 20 characters is generated.
+///   It contains of lowercase letters (default) and symbols.
+/// */
+/// let password = pwg::new(20, &["symbols"]);
+/// println!("{}", password);
+/// ```
+pub fn new(length: usize, ingredients: &[&str]) -> String {
+    let pool = Pool {
+        lowercase_letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+        uppercase_letters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+        numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+        symbols: ["!", "§", "$", "%", "&", "?", "@", "#"],
+    };
+
+    let mut rng = thread_rng();
+
+    let mut password = String::new();
+
+    while password.len() < length {
+        let mut random_strings = ["", "", "", ""];
+
+        {
+            let random_number = rng.gen_range(0, pool.lowercase_letters.len());
+            random_strings[0] = pool.lowercase_letters[random_number];
+        }
+
+        {
+            if ingredients.contains(&"uppercase") {
+                let random_number = rng.gen_range(0, pool.uppercase_letters.len());
+                random_strings[1] = pool.uppercase_letters[random_number];
+            }
+        }
+
+        {
+            if ingredients.contains(&"numbers") {
+                let random_number = rng.gen_range(0, pool.numbers.len());
+                random_strings[2] = pool.numbers[random_number];
+            }
+        }
+
+        {
+            if ingredients.contains(&"symbols") {
+                let random_number = rng.gen_range(0, pool.symbols.len());
+                random_strings[3] = pool.symbols[random_number];
+            }
+        }
+
+        let random_number = rng.gen_range(0, random_strings.len());
+        let random_character = random_strings[random_number];
+
+        password.push_str(random_character);
+    }
+
+    password
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_length() {
+        let password = new(10, &[]);
+        assert_eq!(10, password.len());
+    }
+}
